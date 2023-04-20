773b0632cf5210769f75eef714c2f577af244165
Catherine Östlund <catherine.oestlund@gmail.com>
2023-04-20 15:23:25 +0200

    update: initial screen

---
+++ App.tsx
@@ -0,0 +1,53 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.logo_container}>
+        <Image source={require('./assets/icon.png')} style={styles.logo}></Image>
+      </View>
+      <Text style={styles.text}>App under construction</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center'
+  },
+  text: {
+    fontSize: 30,
+    color: '#fff',
+    marginTop: 40,
+  },
+  logo_container: {
+    marginTop: -120,
+    width: 150,
+    height: 150,
+    backgroundColor: 'rgb(205, 253, 255)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+  },
+  logo: {
+    width: 120, 
+    height: 120
+  },
+});
+
+// const newColorTheme = {
+//   brand: {
+//     white: "#fcfbfb",
+//     dark: "#25292f",
+//     lightblue: "#cdfdff",
+//     darkblue: "#05aabb",
+//     orange: "#fab007"
+//   }
+// }
