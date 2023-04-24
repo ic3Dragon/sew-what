7ab768c4cc9073e396bf92c1709f27ca1bdc6f2c
Catherine Östlund <catherine.oestlund@gmail.com>
2023-04-24 22:00:04 +0200

    add: firebase config and navigation dependencies

--- app.config.js
+++ app.config.js
@@ -1,4 +1,6 @@
-{
+import 'dotenv/config';
+
+export default {
   "expo": {
     "name": "sew-what",
     "slug": "sew-what",
@@ -31,7 +33,13 @@
     "extra": {
       "eas": {
         "projectId": "910b1caf-7c52-460c-880f-7e1ab13188eb"
-      }
+      },
+      firebaseApiKey: process.env.FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.FIREBASE_APP_ID
     },
     "owner": "ic3dragon"
   }
