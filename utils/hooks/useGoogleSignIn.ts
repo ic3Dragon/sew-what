3eb8bacfff6899310f48db217ed49e4652948dd0
Catherine Östlund <catherine.oestlund@gmail.com>
2023-05-31 16:08:40 +0200

    update(useGoogleSignIn): swap to redirect, still broken

--- utils/hooks/useGoogleSignIn.ts
+++ utils/hooks/useGoogleSignIn.ts
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, OAuthCredential, User, getAuth, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, OAuthCredential, User, getAuth, signInWithRedirect } from "firebase/auth";
 
 type SignInError = {
   errorCode: any,
@@ -14,29 +14,31 @@ type Result = {
 }
 
 const auth = getAuth();
-
 const provider = new GoogleAuthProvider();
 
 auth.useDeviceLanguage();
 
-export const useGoogleSignIn = async ():Promise<Result> => {
+export const useGoogleSignIn = async () => { //:Promise<Result>
 
-  const result = await signInWithPopup(auth, provider)
-    .then((res) => {
-      const credential = GoogleAuthProvider.credentialFromResult(res); 
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const token = credential.accessToken;
-      const user = res.user;
-      return { user, token, error: null };
-    }).catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
+  signInWithRedirect(auth, provider)
 
-      return { user: null, token: null, error: {errorCode, errorMessage, email, credential}}
-    });
+  return {auth, provider}
+  // const result = await signInWithPopup(auth, provider)
+  //   .then((res) => {
+  //     const credential = GoogleAuthProvider.credentialFromResult(res); 
+  //     // This gives you a Google Access Token. You can use it to access the Google API.
+  //     const token = credential.accessToken;
+  //     const user = res.user;
+  //     return { user, token, error: null };
+  //   }).catch((error) => {
+  //     const errorCode = error.code;
+  //     const errorMessage = error.message;
+  //     const email = error.customData.email;
+  //     // The AuthCredential type that was used.
+  //     const credential = GoogleAuthProvider.credentialFromError(error);
 
-  return result;
+  //     return { user: null, token: null, error: {errorCode, errorMessage, email, credential}}
+  //   });
+
+  // return result;
 }
