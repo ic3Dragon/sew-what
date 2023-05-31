7a6713f48c48cd243da791db09487faa86e8a8c3
Catherine Östlund <catherine.oestlund@gmail.com>
2023-05-31 15:21:42 +0200

    add(App.tsx): add theme provider to app

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import RootNavigation from './navigation';
+import { ThemeProvider } from 'react-native-elements';
 
 export default function App() {
   return (
-  <RootNavigation />
+    <ThemeProvider>
+      <RootNavigation />
+    </ThemeProvider>
   );
 }
