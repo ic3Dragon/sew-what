b1f518cb2c2856ed2c7877904290aa8844311370
Catherine Östlund <catherine.oestlund@gmail.com>
2023-07-04 11:38:59 +0200

    update: config files

--- app.config.js
+++ app.config.js
@@ -32,7 +32,16 @@ export default {
     "extra": {
       "eas": {
         "projectId": "910b1caf-7c52-460c-880f-7e1ab13188eb"
-      }
+      },
+      firebaseApiKey: process.env.FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.FIREBASE_APP_ID,
+      webClientId: process.env.WEB_CLIENT_ID,
+      androidClientId: process.env.ANDROID_CLIENT_ID,
+      expoClientId: process.env.EXPO_CLIENT_ID
     },
     "owner": "ic3dragon",
     "scheme": "sew-what"
