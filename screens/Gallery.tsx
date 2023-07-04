8f7a3dd284c67bee44c2b161ea4bf649dfd5e81d
Catherine Östlund <catherine.oestlund@gmail.com>
2023-07-04 16:47:17 +0200

    feat(gallery): add search field and initial styling

--- screens/Gallery.tsx
+++ screens/Gallery.tsx
@@ -1,14 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
+import {View, StyleSheet, TextInput} from 'react-native';
 import React from 'react'
+import { COLORS } from '../utils/constants';
 
 const Gallery = () => {
+  const [search, onChangeSearch] = React.useState('');
+
   return (
-    <View>
-      <Text>Gallery</Text>
+    <View style={styles.container}>
+      <View style={styles.filter}>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeSearch}
+          value={search}
+          placeholder="Search    🔍"
+        />
+      </View>
     </View>
   )
 }
 
-export default Gallery
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.light,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    textAlign: 'center',
+    gap: 10 
+  },
+  filter: {
+    marginTop: 20
+  },
+  input: {
+    textAlign: 'center',
+    height: 50,
+    margin: 15,
+    width: 250,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+    fontSize: 20,
+  },
+});
 
-const styles = StyleSheet.create({})
+export default Gallery
