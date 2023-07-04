7a3198c4fd4ff3fdf69cae2a0fa038818c545c83
Catherine Östlund <catherine.oestlund@gmail.com>
2023-07-04 14:43:48 +0200

    feat(Screens): Add basic screens for Gallery and NewPattern

---
+++ screens/NewPattern.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const NewPattern = () => {
+  return (
+    <View>
+      <Text>NewPattern</Text>
+    </View>
+  )
+}
+
+export default NewPattern
+
+const styles = StyleSheet.create({})
