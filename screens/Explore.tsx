fb5c107e8174489968f3132c52ec101569cfa0fe
Catherine Östlund <catherine.oestlund@gmail.com>
2023-07-07 11:10:50 +0200

    feat(more patterns): Add data from waves and wild pattern company

--- screens/Explore.tsx
+++ screens/Explore.tsx
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
 import React from 'react'
 import uuid from "react-native-uuid";
 
 import eamdata from '../patternData/ellieAndMacData.json';
 import p4pdata from '../patternData/p4pData.json';
+import wawdata from '../patternData/wawData.json';
 import PatternGallery from '../components/PatternGallery';
 
-const patternData = [...eamdata, ...p4pdata].map(pattern => ({...pattern, id: uuid.v4().toString()}))
+const patternData = [...wawdata, ...eamdata, ...p4pdata, ].map(pattern => ({...pattern, id: uuid.v4().toString()}))
 
 const Explore = () => {
   return (
